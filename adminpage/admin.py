74ead5e7067aeb5456e4636c0b7b068085ec2265
bagmitapokhrel <bebikapok@gmail.com>
2026-07-23 15:55:40 +0545

    update

--- adminpage/admin.py
+++ adminpage/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Category,Destination, Package, Booking
+from .models import Category,Destination, Package, Booking, Tour
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Destination)
 admin.site.register(Package)
 admin.site.register(Booking)
+admin.site.register(Tour)
 
 
--- adminpage/models.py
+++ adminpage/models.py
@@ -47,8 +47,21 @@ class Booking(models.Model):
     number_of_people = models.PositiveIntegerField()
     travel_date = models.DateField()
     special_requests = models.TextField(blank=True, null=True)
-    booked_at = models.DateTimeField(auto_now_add=True)
+    booked_at = models.DateTimeField(auto_now_add=True, blank=True,null=True)
 
     def __str__(self):
         return f"{self.full_name} - {self.package.title}"
+
+class Tour(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
     
