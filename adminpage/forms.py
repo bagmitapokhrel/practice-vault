f2771dc413308f7aa26d921c08135486a3774e0e
bagmitapokhrel <bebikapok@gmail.com>
2026-08-07 22:21:02 +0545

    thunder

--- adminpage/forms.py
+++ adminpage/forms.py
@@ -11,3 +11,51 @@ class DestinationForm(ModelForm):
     class Meta:
         model = Destination
         fields = '__all__'  
+
+from django import forms
+from .models import TravelPlan
+
+class TravelPlanForm(forms.ModelForm):
+
+    class Meta:
+        model = TravelPlan
+        fields = [
+            "destination",
+            "travel_date",
+            "travelers",
+            "budget",
+            "hotel",
+            "transport",
+            "meals",
+            "special_requests",
+        ]
+
+        widgets = {
+            "travel_date": forms.DateInput(
+                attrs={
+                    "type": "date",
+                    "class": "form-control"
+                }
+            ),
+
+            "travelers": forms.NumberInput(
+                attrs={
+                    "min": 1,
+                    "class": "form-control"
+                }
+            ),
+
+            "budget": forms.NumberInput(
+                attrs={
+                    "placeholder": "Enter your budget",
+                    "class": "form-control"
+                }
+            ),
+
+            "special_requests": forms.Textarea(
+                attrs={
+                    "rows": 4,
+                    "placeholder": "Special requests..."
+                }
+            ),
+        }        
