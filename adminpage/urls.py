c22b024fe2ebcc199385c12dd996ce863058d674
bagmitapokhrel <bebikapok@gmail.com>
2026-08-08 21:40:34 +0545

    A

--- adminpage/urls.py
+++ adminpage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PackageCreateView, DestinationCreateView
+from .views import PackageCreateView, DestinationCreateView, travel_plan
 
 urlpatterns = [
 
     path('packages/create/', PackageCreateView, name='package_create'),
     path('destinations/create/', DestinationCreateView, name='destination_create'),
+    path('travel-plans/', travel_plan, name='travel_plan'),
 ]
